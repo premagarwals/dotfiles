2f61813ed9d5e6306f034169e58fc5a133f439c1
premagarwals <prem12937@gmail.com>
2025-04-21 03:36:48 +0530

    feat: no auto-accepting unwanted suggestions when pressed enter

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -14,12 +14,31 @@ return {
       end
     end, { desc = "Options | Toggle Autocomplete" })
   end,
-  opts = function(_, opts)
+opts = function(_, opts)
     -- table.insert(opts.sources, 2, { name = "codeium" })
     -- table.insert(opts.sources, 1, { name = "supermaven" })
 
     opts.mapping = vim.tbl_extend("force", {}, opts.mapping, {
-      -- You can add here new mappings.
+      -- Your mapping to accept suggestion with Ctrl+.
+      ['<C-.>'] = require("cmp").mapping(function(fallback)
+        if require("cmp").visible() then
+          require("cmp").confirm({ select = true }) -- Accept the currently selected item
+        else
+          fallback() -- If no completion is visible, do nothing or perform default behavior
+        end
+      end, { 'i', 's' }), -- Apply in Insert and Select modes
+
+      -- *** ADD or ENSURE this mapping for Enter exists ***
+      -- For Enter, just move to the next line (do NOT accept completion)
+      ['<CR>'] = require("cmp").mapping(function(fallback)
+        -- If completion menu is visible or not, Enter will just perform a newline.
+        -- If you want to close the menu on Enter without selecting, you could add cmp.abort() here.
+        fallback()
+      end, { 'i', 's' }), -- Apply in Insert and Select modes
+
+      -- Add your other mappings here if needed, e.g., for Tab navigation
+      -- ['<Tab>'] = require("cmp").mapping(function(fallback) ... end, { 'i', 's' }),
+
     })
 
     opts.completion["completeopt"] = "menu,menuone,noselect" -- disable autoselect
@@ -27,7 +46,7 @@ return {
     opts.enabled = function()
       return (vim.g.toggle_cmp and vim.bo.buftype == "")
     end
-
+  
     require("luasnip").filetype_extend("javascriptreact", { "html" })
     require("luasnip").filetype_extend("typescriptreact", { "html" })
     require("luasnip").filetype_extend("svelte", { "html" })
@@ -56,7 +75,7 @@ return {
       "hrsh7th/cmp-cmdline",
       config = function()
         local cmdline_mappings = vim.tbl_extend("force", {}, require("cmp").mapping.preset.cmdline(), {
-          ["<CR>"] = { c = require("cmp").mapping.confirm { select = true } },
+          ["<C-]>"] = { c = require("cmp").mapping.confirm { select = true } },
         })
 
         require("cmp").setup.cmdline(":", {
